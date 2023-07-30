bac9d81042ca8a9b7f1ad79b3de46caf052bc2eb
Boniface <vermi1iongodv@gmail.com>
2023-07-30 22:45:22 +0300

    barbie first change

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+  // Get the first button and add a click event listener
+  const buyButton1 = document.getElementById('buyButton1');
+  buyButton1.addEventListener('click', () => {
+    const buyLink = 'https://pancakeswap.finance/swap?inputCurrency=0x55d398326f99059fF775485246999027B3197955&outputCurrency=0x19EF6e5FBb5962a14b506dD163d50bEb915CF759';
+    window.open(buyLink, '_blank');
+  });
+
+  // Get the second button and add a click event listener
+  const buyButton2 = document.getElementById('buyButton2');
+  buyButton2.addEventListener('click', () => {
+    const buyLink = 'https://pancakeswap.finance/swap?inputCurrency=0x55d398326f99059fF775485246999027B3197955&outputCurrency=0x19EF6e5FBb5962a14b506dD163d50bEb915CF759';
+    window.open(buyLink, '_blank');
+  });
+
+  // Get the third button and add a click event listener
+  const buyButton3 = document.getElementById('buyButton3');
+  buyButton3.addEventListener('click', () => {
+    const buyLink = 'https://pancakeswap.finance/swap?inputCurrency=0x55d398326f99059fF775485246999027B3197955&outputCurrency=0x19EF6e5FBb5962a14b506dD163d50bEb915CF759';
+    window.open(buyLink, '_blank');
+  });
+
