9cb1f4a859d4699492f4c7aedc7b018361256305
Boniface <vermi1iongodv@gmail.com>
2023-08-07 13:26:38 +0300

    debugging

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,44 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+
+ 
+
 
 function compileSass() {
-  return gulp.src('./src/scss/*.scss')
+  return gulp.src('./src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css')); // 
+    .pipe(gulp.dest('.dist/css/'));
+
 }
 
 function watchSass() {
-  gulp.watch('./src/scss/*.scss', compileSass);
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
 }
 
 gulp.task('sass', compileSass);
 gulp.task('watch', watchSass);
 gulp.task('default', gulp.series('sass', 'watch'));
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+    port: 5000 
+  });
+
+  
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass')).on('change', browserSync.reload)
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  
+});
+
+
+
+
+
 
 
 
